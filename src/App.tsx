af9717e835224e137fc5400b9099397b4a213fe0
Evgeny <evgeny.efimenko@proton.me>
2024-03-01 17:41:11 +0500

    chore: update tabble login

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md'
 import { useQuery } from 'react-query'
 import axios from 'axios'
 import { md5 } from 'js-md5'
-import { getTimestamp } from './utils'
+import { getTimestamp, removeDuplicates, removeDuplicatesWithSet } from './utils'
 import { useState } from 'react'
 
-async function fetchCoins() {
+async function fetchCoins(skip: number) {
   const xAuthHeader = md5(`${import.meta.env.VITE_AUTH_PASS}_${getTimestamp()}`)
 
-  const { data: idsResponse } = await axios.post(
+  let { data: idsResponse } = await axios.post(
     `${import.meta.env.VITE_API_URL}`,
     {
       action: 'get_ids',
-      params: { offset: 0, limit: 100 },
+      params: { offset: skip, limit: 5 },
     },
     {
       headers: {
@@ -21,12 +21,13 @@ async function fetchCoins() {
       },
     }
   )
+  idsResponse = removeDuplicates(idsResponse.result)
 
   const { data: products } = await axios.post(
     `${import.meta.env.VITE_API_URL}`,
     {
       action: 'get_items',
-      params: { ids: idsResponse.result },
+      params: { ids: idsResponse },
     },
     {
       headers: {
@@ -34,25 +35,19 @@ async function fetchCoins() {
       },
     }
   )
-  console.log(products.result)
+  console.log(idsResponse)
+  console.log(products)
 
   return products.result
 }
 
 function App() {
-  const { data, isLoading, isError } = useQuery('products', fetchCoins)
   const [selectedFilterMethod, setSelectedFilterMethod] = useState('product')
-
-  if (isLoading) {
-    return <h3>Loading</h3>
-  }
+  const [page, setPage] = useState(0)
+  const { data, isLoading, isError } = useQuery(['products', page], () => fetchCoins(page))
 
   if (isError) {
     return <h3>Error</h3>
-  }
-
-  if (!data) {
-    return <h3>no data</h3>
   }
 
   const filterBy: { value: string; label: string }[] = [
@@ -63,7 +58,7 @@ function App() {
 
   return (
     <>
-      <main className='max-w-screen-lg mx-auto px-4 flex flex-col justify-center min-h-full py-16'>
+      <main className='max-w-screen-lg mx-auto px-4 min-h-full py-16'>
         <section>
           <fieldset className='grid w-full grid-cols-12 items-end'>
             {filterBy.map((item) => (
@@ -71,7 +66,7 @@ function App() {
                 className={`${selectedFilterMethod === item.value ? 'bg-gray-200' : ''} w-full p-3 rounded-md ${item.value === 'price' ? 'col-span-2' : 'col-span-4'}`}
               >
                 <label
-                  className={`block mb-2 text-sm text-gray-900 cursor-pointer ${selectedFilterMethod === item.value ? 'font-semibold' : 'font-medium '}`}
+                  className={`block mb-2 text-sm cursor-pointer ${selectedFilterMethod === item.value ? 'font-semibold text-foreground' : 'font-medium text-foreground-lighter'}`}
                   onClick={() => setSelectedFilterMethod(item.value)}
                 >
                   {item.label}
@@ -100,26 +95,28 @@ function App() {
             </p>
           </div>
           <div className='flex shadow bg-card rounded-lg ring-1 ring-black/[.05] overflow-hidden'>
-            <button className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
+            <button
+              onClick={() => setPage((p) => p - 10)}
+              className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
               <MdNavigateBefore className='h-6 w-6' />
             </button>
-            <button className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
+            <button
+              onClick={() => setPage((p) => p + 10)}
+              className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
               <MdNavigateNext className='h-6 w-6' />
             </button>
           </div>
         </div>
         <div className='mt-8'>
-          <Table data={data} columns={['hi', 'hello']} />
+          {isLoading ? (
+            'loading'
+          ) : (
+            <Table data={data} columns={['hi', 'hello']} />
+          )}
+
+
         </div>
 
-        <div className='flex shadow bg-card rounded-lg ring-1 ring-black/[.05] overflow-hidden self-end mt-8'>
-          <button className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
-            <MdNavigateBefore className='h-6 w-6' />
-          </button>
-          <button className='h-8 w-8 flex justify-center items-center hover:bg-card-hover'>
-            <MdNavigateNext className='h-6 w-6' />
-          </button>
-        </div>
       </main>
     </>
   )
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export function getTimestamp() {
   return new Date().toISOString().slice(0, 10).replace(/-/g, '')
 }
+
+export function removeDuplicates(arr: string[]): string[] {
+    return Array.from(new Set(arr));
+}
